72dcf13d0ef413ef916bd4df646ad121c1ea1260
shicw <shicw@pt.com>
2018-03-27 15:45:00 +0800

    second

--- application/index/controller/NewsDetail.php
+++ application/index/controller/NewsDetail.php
@@ -19,26 +19,28 @@ class NewsDetail extends UserBaseController
         $user = session('user');
 
         $id = $this->request->param('id');
+        $table = Db::name('news');
         //将news表新闻的view_num字段加1
-        Db::name('news')->where(['id'=>$id,'delete_time'=>0])->setInc('view_num',1);
+        $table->where(['id'=>$id,'delete_time'=>0])->setInc('view_num',1);
         //查询新闻详情
-        $data = Db::name('news n')->join([
-            ['department d','d.id=n.dep_id'],
-            ['news_type nt','nt.id=n.type_id'],
-            ['staff s','s.id=n.publisher']
-        ])->field(['n.*','d.name dep','nt.name type','s.name publisher'])
+        $data = $table->alias('n')->join('staff s','s.id=n.publisher')
+            ->field(['n.*','s.name publisher'])
             ->where(['n.id'=>$id,'n.delete_time'=>0])->find();
+        //查询当前用户是否收藏该新闻
+        $collect = Db::name('collection')->where(['staff_id'=>$user['id'],'news_id'=>$id,'delete_time'=>0])->find();
         //查询新闻评论列表,关联staff表得到头像
         $comment = Db::name('comment c')->join('staff s','s.id=c.staff_id')
             ->field(['c.*','s.avatar'])->where(['c.delete_time'=>0,'news_id'=>$id])
             ->order('c.create_time desc')->paginate(10);
         //获取分页显示
         $page = $comment->render();
+        //查询点赞数
         $this->assign([
             'data'=>$data,
             'nav' => $nav,
             'user' => $user,
             'comment' => $comment,
+            'collect' => $collect,
             'page' => $page
         ]);
         return $this->fetch();
@@ -54,8 +56,6 @@ class NewsDetail extends UserBaseController
                 'comment.require'   => '评论不能为空',
             ]);
             $data = $this->request->post();
-            //$newsId = $this->request->param('newsId');
-            //$comment = $this->request->param('comment');
             if (!$validate->check($data)) {
                 $this->error($validate->getError());
             }
@@ -80,4 +80,44 @@ class NewsDetail extends UserBaseController
             $this->error("请求错误!");
         }
     }
+    //收藏 0：未登录 1：收藏成功 2：收藏失败 3：取消收藏 4：取消失败
+    public function collect($id){
+        $userId = session('user.id');
+        $code = null;
+        //如果用户未登录，返回0
+        if (!isset($userId)){
+            return json(['code' => 0]);
+        }else{
+            $news = Db::name('news')->where(['delete_time'=>0,'id'=>$id])->find();
+            //查询当前用户是否收藏过改新闻，是则取消收藏，否则添加收藏
+            $table = Db::name('collection');
+            $find = $table->where(['staff_id'=>$userId,'news_id'=>$id,'delete_time'=>0])->find();
+            if($find){//取消收藏
+                $result = $table->where(['staff_id'=>$userId,'news_id'=>$id,'delete_time'=>0])->setField('delete_time',time());
+                $code = $result ? 3 : 4;
+                //操作成功写入日志，此处是返回给ajax，不方便调用success方法来写日志
+                if($code == 3) Db::name('logs')->insert(['msg'=>'取消收藏成功！','data'=>'取消收藏：'.$news['title'],'staff_id'=>$userId]);
+            }else{//添加收藏
+                $result = $table->insert(['staff_id'=>$userId,'news_id'=>$id,'create_time'=>time()]);
+                $code = $result ? 1 : 2;
+                if($code == 1) Db::name('logs')->insert(['msg'=>'添加收藏成功！','data'=>'添加收藏：'.$news['title'],'staff_id'=>$userId]);
+
+            }
+            return json(['code' => $code]);
+        }
+    }
+    //点击头像查看用户信息 ajax
+    public function showStaff($id){
+        $data = Db::name('staff s')->join([
+            ['department d','d.id=s.dep_id'],
+            ['jobs j','j.id=s.job_id'],
+        ])->field(['s.email','s.mobile','d.name dep','j.name job'])
+            ->where(['s.delete_time'=>0,'s.id'=>$id])->find();
+        $code = $data ? 1 : 0;
+        return json([
+            'code' => $code,
+            'data' => $data
+        ]);
+    }
+
 }
